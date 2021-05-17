30b73ae14b102bf14d19c2a2f0576277c07386e4
Denys Dnishchenko <ddnishchenko@itera-research.com>
2021-05-17 16:45:39 +0300

    revert change

--- apps/walkthrough-panorama/src/app/model-data.resolver.ts
+++ apps/walkthrough-panorama/src/app/model-data.resolver.ts
@@ -1,27 +1,21 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Project } from './interfaces/project';
-import { loadProjects } from './projects/state/projects.actions';
-import { ProjectsState } from './projects/state/projects.reducer';
-import { selectProjectsState } from './projects/state/projects.selectors';
+import { ProjectsService } from './projects/service/projects.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ModelDataResolver implements Resolve<ProjectsState> {
+export class ModelDataResolver implements Resolve<Project> {
 
-  constructor(private store: Store<ProjectsState>) { }
+  constructor(private projectService: ProjectsService) { }
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-  ): Observable<ProjectsState> {
+  ): Observable<Project> {
     const projectId = route.paramMap.get('id');
-    this.store.dispatch(loadProjects())
-    return this.store.pipe(
-      select(selectProjectsState)
-    );
+    return this.projectService.getPanoramas(projectId);
   }
 }
