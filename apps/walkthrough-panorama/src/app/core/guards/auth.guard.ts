b6bc843a5bd11ee3b05fb6a5edcd8b7d70b43a16
Denys Dnishchenko <ddnishchenko@itera-research.com>
2021-09-07 14:21:30 +0300

    redirect not authenticated

--- apps/walkthrough-panorama/src/app/core/guards/auth.guard.ts
+++ apps/walkthrough-panorama/src/app/core/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { CanActivate, CanLoad } from "@angular/router";
+import { CanActivate, CanLoad, Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 
 
@@ -7,14 +7,24 @@ import { AuthService } from "../../services/auth.service";
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
   private get isAuthenticated() {
     return !!this.authService.accessToken;
   }
+  private checkAuth() {
+    if (!this.isAuthenticated) {
+      this.router.navigate(['auth'])
+    }
+  }
   canActivate(route, state) {
+    this.checkAuth();
     return this.isAuthenticated;
   }
   canLoad(route, segments) {
+    this.checkAuth();
     return this.isAuthenticated;
   }
 }
