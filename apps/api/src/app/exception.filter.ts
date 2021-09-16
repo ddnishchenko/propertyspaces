19ae6e04657500475b02d0a4cb397970652fbdc8
Denys Dnishchenko <ddnishchenko@itera-research.com>
2021-09-16 15:40:28 +0300

    Add request validation message

--- apps/api/src/app/exception.filter.ts
+++ apps/api/src/app/exception.filter.ts
@@ -3,12 +3,12 @@ import { BaseExceptionFilter } from '@nestjs/core';
 
 @Catch(HttpException)
 export class AllExceptionsFilter extends BaseExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: HttpException | any, host: ArgumentsHost) {
     // super.catch(exception, host);
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
-
+    console.log(exception);
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
@@ -19,11 +19,14 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
         ? exception.message
         : 'Internal server error';
 
+    const res = exception.response ? exception.response : {};
+
     response.status(status).json({
       statusCode: status,
       message,
       timestamp: new Date().toISOString(),
       path: request.url,
+      ...res
     });
   }
 }
