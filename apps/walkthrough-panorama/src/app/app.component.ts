7ba81612f3576824f0bdb9a6b0a27b64fa773908
Denys Dnishchenko <ddnishchenko@itera-research.com>
2021-09-29 18:28:25 +0300

    Service worker

--- apps/walkthrough-panorama/src/app/app.component.ts
+++ apps/walkthrough-panorama/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CheckForUpdatesService } from './services/check-for-updates.service';
 
 @Component({
   selector: 'propertyspaces-root',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private checkForUpdate: CheckForUpdatesService) { }
+}
